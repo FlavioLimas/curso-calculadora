955858697f8b4cd3192e8e1bbad9812a0a8b8a67
Flavio Lima <flaviolima.s@live.com>
2021-06-23 19:49:08 -0300

    Definindo cor

--- src/br/com/calc/visao/Display.java
+++ src/br/com/calc/visao/Display.java
@@ -1,7 +1,13 @@
 package br.com.calc.visao;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 public class Display extends JPanel {
+
+    public Display() {
+        setBackground(Color.GREEN);
+    }
     
 }
