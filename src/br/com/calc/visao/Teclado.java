26b8a7bf08adf26c47f2ee8de8dd7a8c2d2a3ac0
Flavio Lima <flaviolima.s@live.com>
2021-06-23 19:49:56 -0300

    definindo construtor e cor

--- src/br/com/calc/visao/Teclado.java
+++ src/br/com/calc/visao/Teclado.java
@@ -1,7 +1,13 @@
 package br.com.calc.visao;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 public class Teclado extends JPanel {
+
+    public Teclado() {
+        setBackground(Color.RED);
+    }
     
 }
