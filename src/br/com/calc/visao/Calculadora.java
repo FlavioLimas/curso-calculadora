e03bef7fa19983929a23006e85dfc8f1b746a984
Flavio Lima <flaviolima.s@live.com>
2021-06-23 19:50:44 -0300

    definindo organizador de layout

--- src/br/com/calc/visao/Calculadora.java
+++ src/br/com/calc/visao/Calculadora.java
@@ -5,11 +5,16 @@ import javax.swing.JFrame;
 public class Calculadora extends JFrame {
 
     public Calculadora() {
+
+        organizarLayout();
         
         setSize(232, 322);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private void organizarLayout() {
     }
 
     public static void main(String[] args) {
