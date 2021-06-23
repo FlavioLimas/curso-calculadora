dca1f735e441d0c9f9f401d454e8130135fa3d66
Flavio Lima <flaviolima.s@live.com>
2021-06-23 19:39:07 -0300

    finalizando app

--- src/br/com/calc/visao/Calculadora.java
+++ src/br/com/calc/visao/Calculadora.java
@@ -5,8 +5,8 @@ import javax.swing.JFrame;
 public class Calculadora extends JFrame {
 
     public Calculadora() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
-        
     }
 
     public static void main(String[] args) {
