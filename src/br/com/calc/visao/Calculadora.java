b1f7dbde7be4b5ba03988d69969a80324cfa5492
Flavio Lima <flaviolima.s@live.com>
2021-06-23 19:35:29 -0300

    definindo construtor e main

--- src/br/com/calc/visao/Calculadora.java
+++ src/br/com/calc/visao/Calculadora.java
@@ -4,6 +4,13 @@ import javax.swing.JFrame;
 
 public class Calculadora extends JFrame {
 
-    
+    public Calculadora() {
+        setVisible(true);
+        
+    }
+
+    public static void main(String[] args) {
+        new Calculadora();
+    }
     
 }
