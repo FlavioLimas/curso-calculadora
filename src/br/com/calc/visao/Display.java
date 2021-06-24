bc81c10b829f4cac9e2ccfdd61537cababd340a7
Flavio Lima <flaviolima.s@live.com>
2021-06-24 17:16:10 -0300

    definindo atributo e instancia no construtor

--- src/br/com/calc/visao/Display.java
+++ src/br/com/calc/visao/Display.java
@@ -1,10 +1,14 @@
 package br.com.calc.visao;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class Display extends JPanel {
 
+    private final JLabel label;
+    
     public Display() {
+        label = new JLabel();
     }
     
 }
