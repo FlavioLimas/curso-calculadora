4498789b24b3751be39146598ec80a98fde6e15c
Flavio Lima <flaviolima.s@live.com>
2021-06-24 18:09:27 -0300

    centralizando a direita

--- src/br/com/calc/visao/Display.java
+++ src/br/com/calc/visao/Display.java
@@ -1,6 +1,7 @@
 package br.com.calc.visao;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 
 import javax.swing.JLabel;
@@ -15,7 +16,9 @@ public class Display extends JPanel {
         label = new JLabel("1234,56");
         label.setForeground(Color.WHITE);
         label.setFont(new Font("courier", Font.PLAIN, 30));
-        
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT));
+
         add(label);
     }
     
