b70bc08a2c4b9bd63d47b2b250b38ff7e04895ee
Flavio Lima <flaviolima.s@live.com>
2021-06-24 18:36:53 -0300

    import

--- src/br/com/calc/visao/Botao.java
+++ src/br/com/calc/visao/Botao.java
@@ -1,10 +1,13 @@
 package br.com.calc.visao;
 
+import java.awt.Color;
 import javax.swing.JButton;
 
 public class Botao extends JButton {
     
-    public Botao() {
-        
+    public Botao(String texto, Color cor) {
+        setText(texto);
+        setOpaque(true);
+        setBackground(cor);
     }
 }
