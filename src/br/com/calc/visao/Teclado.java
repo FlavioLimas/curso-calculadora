4a933afb0624f5d5518933d5ff9d2162b2d06eed
Flavio Lima <flaviolima.s@live.com>
2021-06-24 21:38:13 -0300

    disposicao do botoes no teclado

--- src/br/com/calc/visao/Teclado.java
+++ src/br/com/calc/visao/Teclado.java
@@ -1,9 +1,8 @@
 package br.com.calc.visao;
 
 import java.awt.Color;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JPanel;
 
@@ -18,32 +17,41 @@ public class Teclado extends JPanel {
         GridBagLayout layout = new GridBagLayout();
         GridBagConstraints c = new GridBagConstraints();
 
-        setLayout(new GridLayout(5, 4));
+        setLayout(layout);
+        
+        adicionarBotao("AC", COR_CINZA_ESCURO, c, 0, 0);
+        adicionarBotao("+/-", COR_CINZA_ESCURO, c, 1, 0);
+        adicionarBotao("%", COR_CINZA_ESCURO, c, 2, 0);
+        adicionarBotao("/", COR_LARANJA, c, 3, 0);
+        
+        adicionarBotao("7", COR_CINZA_CLARO, c, 0, 1);
+        adicionarBotao("8", COR_CINZA_CLARO, c, 1, 1);
+        adicionarBotao("9", COR_CINZA_CLARO, c, 2, 1);
+        adicionarBotao("*", COR_LARANJA, c, 3, 1);
+        
+        adicionarBotao("4", COR_CINZA_CLARO, c, 0, 2);
+        adicionarBotao("5", COR_CINZA_CLARO, c, 1, 2);
+        adicionarBotao("6", COR_CINZA_CLARO, c, 2, 2);
+        adicionarBotao("-", COR_LARANJA, c, 3, 2);
+        
+        adicionarBotao("1", COR_CINZA_CLARO, c, 0, 3);
+        adicionarBotao("2", COR_CINZA_CLARO, c, 1, 3);
+        adicionarBotao("3", COR_CINZA_CLARO, c, 2, 3);
+        adicionarBotao("+", COR_LARANJA, c, 3, 3);
+        
+        adicionarBotao("0", COR_CINZA_CLARO, c, 0, 4);
+        adicionarBotao("0", COR_CINZA_CLARO, c, 1, 4);
+        adicionarBotao(",", COR_CINZA_CLARO, c, 2, 4);
+        adicionarBotao("=", COR_LARANJA, c, 3, 4);
 
-        add(new Botao("AC", COR_CINZA_ESCURO), c);
-        add(new Botao("+/-", COR_CINZA_ESCURO), c);
-        add(new Botao("%", COR_CINZA_ESCURO), c);
-        add(new Botao("/", COR_LARANJA), c);
+    }
+
+    private void adicionarBotao(String texto, Color cor, GridBagConstraints c, int x, int y) {
         
-        add(new Botao("7", COR_CINZA_CLARO), c);
-        add(new Botao("8", COR_CINZA_CLARO), c);
-        add(new Botao("9", COR_CINZA_CLARO), c);
-        add(new Botao("*", COR_LARANJA), c);
-        
-        add(new Botao("6", COR_CINZA_CLARO), c);
-        add(new Botao("5", COR_CINZA_CLARO), c);
-        add(new Botao("4", COR_CINZA_CLARO), c);
-        add(new Botao("-", COR_LARANJA), c);
-        
-        add(new Botao("3", COR_CINZA_CLARO), c);
-        add(new Botao("2", COR_CINZA_CLARO), c);
-        add(new Botao("1", COR_CINZA_CLARO), c);
-        add(new Botao("+", COR_LARANJA), c);
-        
-        add(new Botao("0", COR_CINZA_CLARO), c);
-        add(new Botao("", COR_CINZA_CLARO), c);
-        add(new Botao(",", COR_CINZA_CLARO), c);
-        add(new Botao("=", COR_LARANJA), c);
+        c.gridx = x;
+        c.gridy = y;
+        Botao botao = new Botao(texto, cor);
+        add(botao, c);
     }
     
 }
