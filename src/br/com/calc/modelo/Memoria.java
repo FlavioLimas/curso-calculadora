d3ade1aeab08cf12d8fad28ca58dd42408574923
Flavio Lima <flaviolima.s@live.com>
2021-06-28 19:35:07 -0300

    registrando observador

--- src/br/com/calc/modelo/Memoria.java
+++ src/br/com/calc/modelo/Memoria.java
@@ -1,8 +1,13 @@
 package br.com.calc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Memoria {
 
     private static final Memoria instancia = new Memoria();
+
+    private final List<MemoriaObservador> observadores = new ArrayList<>();
 
     private String textoAtual = "";
     
@@ -12,6 +17,10 @@ public class Memoria {
 
     public static Memoria getInstancia() {
         return instancia;
+    }
+
+    public void adicionarObservador(MemoriaObservador observador) {
+        observadores.add(observador);
     }
 
     public String getTextoAtual() {
