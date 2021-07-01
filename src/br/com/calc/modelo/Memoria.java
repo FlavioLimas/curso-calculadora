27a67795946e27d5ec4b65e42d7c24ce48181144
Flavio Lima <flaviolima.s@live.com>
2021-07-01 20:30:08 -0300

    botao positivo negativo

--- src/br/com/calc/modelo/Memoria.java
+++ src/br/com/calc/modelo/Memoria.java
@@ -7,6 +7,7 @@ public class Memoria {
 
     private enum TipoComando {
         ZERAR,
+        SINAL,
         NUMERO,
         DIV,
         MULT,
@@ -52,6 +53,10 @@ public class Memoria {
             textoBuffer = "";
             substituir = false;
             ultimaOperacao = null;
+        } else if (tipoComando == TipoComando.SINAL && textoAtual.contains("-")) {
+            textoAtual = textoAtual.substring(1);
+        } else if (tipoComando == TipoComando.SINAL && !textoAtual.contains("-")) {
+            textoAtual = "-" + textoAtual;
         } else if(tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
             textoAtual = substituir ? texto : textoAtual + texto;
             substituir = false;
@@ -113,6 +118,8 @@ public class Memoria {
                 return TipoComando.SUB;
             } else if ("=".equals(texto)) {
                 return TipoComando.IGUAL;
+            } else if ("±".equals(texto)) {
+                return TipoComando.SINAL;
             } else if (",".equals(texto) && !textoAtual.contains(",")) {
                 return TipoComando.VIRGULA;
             }
