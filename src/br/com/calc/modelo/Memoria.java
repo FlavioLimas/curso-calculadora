54cbc0cd7f0d6b3bbe49aec11eb2610429683242
Flavio Lima <flaviolima.s@live.com>
2021-06-30 21:45:16 -0300

    captando a segunda operacao

--- src/br/com/calc/modelo/Memoria.java
+++ src/br/com/calc/modelo/Memoria.java
@@ -56,6 +56,8 @@ public class Memoria {
         } else if(tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
             textoAtual = substituir ? texto : textoAtual + texto;
             substituir = false;
+        } else {
+            substituir = true;
         }
     }
 
